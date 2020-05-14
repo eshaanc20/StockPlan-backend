e3f5653b28b3a74dc991bb4ac476872deca0711c
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-05-14 02:09:41 -0400

    route for login

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,15 @@ var {user} = require('../db/mongoose.js');
 
 //authentication for login
 router.post('/login', function(req, res, next) {
+    user.findOne({email: req.body.email}).then(document => {
+        if (document.password == req.body.password) {
+            res.send(true);
+        } else {
+            res.send(false);
+        }
+    }).catch(err => {
+        res.status(404).send('Error')
+    })
 });
 
 //create new user
@@ -17,7 +26,7 @@ router.post('/', function(req, res, next) {
     newUser.save().then(() => {
         res.send('Successful');
     }).catch(err => {
-        res.status(404).send('User was not registered')
+        res.status(404).send('Error')
     })
 });
 
