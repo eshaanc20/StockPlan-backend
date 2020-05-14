c5e15b09f655f15e50ec1b2c96fb21c942d50ff0
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-05-13 23:41:09 -0400

    updated

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,12 +1,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+mongoose.connect('', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 const dataFormat = new Schema({
     firstName: String,
     lastName: String,
     email: String,
     password: String,
-    stocks: Array
+    stocks: Array,
+    goals: Array
 });
 
 var users = mongoose.model('user', dataFormat);
