bd55d5f248d596d159a86aabdf3e4edb335c8f01
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-05-22 23:08:17 -0400

    Added goal routes to app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
+var goalRouter = require('./routes/goal');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
+app.use('/goal', goalRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
