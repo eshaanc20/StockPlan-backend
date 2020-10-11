14663d109b54dee3e81a68a519eee4cb3d2c6467
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-10-11 17:06:06 -0400

    Added more calculations

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -32,24 +32,34 @@ router.get('/:id', authentication, async function(req, res, next) {
             amountDifference = (Math.round(amountDifference * 100))/100
             let changeDirection = (response.data.c - response.data.o) > 0? "increase": "decrease";
             let moreDataResponse = await axios.get('https://finnhub.io/api/v1/stock/metric?symbol=' + stock + '&metric=all&token=btpsg2n48v6rdq37lt60');
+            let highDaily = Math.round(response.data.h * 100) / 100;
+            let lowDaily = Math.round(response.data.l * 100) / 100;
+            let high52WeekPrice = Math.round(moreDataResponse.data.metric['52WeekHigh'] * 100) / 100;
+            let low52WeekPrice = Math.round(moreDataResponse.data.metric['52WeekLow'] * 100) / 100;
+            let marketValue = moreDataResponse.data.metric.marketCapitalization / 1000;
+            let epsNumber = Math.round(moreDataResponse.data.metric.epsNormalizedAnnual * 100) / 100;
+            let dividentYieldNumber = Math.round(moreDataResponse.data.metric.dividendYieldIndicatedAnnual * 100) / 100;
+            let epsGrowthNumber = Math.round(moreDataResponse.data.metric.epsGrowth5Y * 100) / 100;
+            let profitEarningNumber = Math.round(moreDataResponse.data.metric.peNormalizedAnnual * 100) / 100;
+            let netMarginNumber = Math.round(moreDataResponse.data.metric.netProfitMarginAnnual * 100) / 100;
             const info = {
                 symbol: stock,
                 current: response.data.c,
                 open: response.data.o,
-                high: response.data.h,
-                low: response.data.l,
+                high: highDaily,
+                low: lowDaily,
                 previousClosePrice: response.data.pc,
                 change: changeDirection,
                 percentChange: changeAmount,
                 amountChange: amountDifference,
-                high52Week: moreDataResponse.data.metric['52WeekHigh'],
-                low52Week: moreDataResponse.data.metric['52WeekLow'],
-                marketCap: moreDataResponse.data.metric.marketCapitalization,
-                eps: moreDataResponse.data.metric.epsNormalizedAnnual,
-                epsGrowth: moreDataResponse.data.metric.epsGrowth5Y,
-                dividendYield: moreDataResponse.data.metric.dividendYieldIndicatedAnnual,
-                profitEarningRatio: moreDataResponse.data.metric.peNormalizedAnnual,
-                netProfitMargin: moreDataResponse.data.metric.netProfitMarginAnnual
+                high52Week: high52WeekPrice,
+                low52Week: low52WeekPrice,
+                marketCap: marketValue,
+                eps: epsNumber,
+                epsGrowth: epsGrowthNumber,
+                dividendYield: dividentYieldNumber,
+                profitEarningRatio: profitEarningNumber,
+                netProfitMargin: netMarginNumber
             }
             stocks.push(info);
         };
