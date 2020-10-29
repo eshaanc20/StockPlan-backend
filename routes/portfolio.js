3f47e2a15edbdfd193bebf8ca83a4c143775969b
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-10-28 21:39:50 -0400

    Added route for adding to portfolio

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,1 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var {User} = require('../db/mongoose.js');
+var {Goal} = require('../db/mongoose.js');
+var {Portfolio} = require('../db/mongoose.js');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+var authentication = require('../middleware/authentication.js');
+var axios = require('axios');
 
+router.post('/add', authentication, async function(req, res, next) {
+    try {
+        const info = {
+            symbol: req.body.symbol,
+            shares: req.body.shares,
+            price: req.body.price,
+            userId: req.user._id
+        }
+        const portfolio = new Portfolio(info);
+        await portfolio.save();
+        res.send({requestStatus: true});
+    } catch {
+        res.status(404).send({requestStatus: false});
+    }
+});
